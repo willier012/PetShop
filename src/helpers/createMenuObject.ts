a0969a94d3f9e1ca7d37c0af56eff7f51c9f51b4
WILLIER GONCALVES <CG3038203@car.ifsp>
2026-04-01 08:07:41 -0300

    projetofinalizado

---
+++ src/helpers/createMenuObject.ts
@@ -0,0 +1,15 @@
+type MenuOptions = '' | 'all' | 'dogs' | 'cats' | 'fishes'
+
+export const createMenuObject = (activeMenu: MenuOptions) => {
+let returnObject = {
+all: false,
+dogs: false,
+cats: false,
+fishes: false
+}
+if (activeMenu !== '') {
+returnObject[activeMenu] = true;
+}
+
+return returnObject
+}
